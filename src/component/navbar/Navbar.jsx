4b0471041c8f0ed5698e80fd7c2b6d4ed3106b9d
YuiChen0207 <asd0935252359@gmail.com>
2023-06-11 19:13:32 +0800

    Merge branch 'main' of https://github.com/OwenLu0125/Simple-Twitter into feature/add-pop-up-model
    # Conflicts:
    #	src/component/navbar/Navbar.jsx
    
    feat: seperate navitems from navbar

--- src/component/navbar/Navbar.jsx
+++ src/component/navbar/Navbar.jsx
@@ -9,24 +9,31 @@ import NavbarUserIconActive from "../../assets/userInfoActive.svg";
 import NavbarSettingIcon from "../../assets/setting.svg";
 import NavbarSettingIconActive from "../../assets/settingActive.svg";
 import "./Navbar.scss";
+import NavbarItem from "./navItems/NavItems";
 
 const navItems = [
   {
     path: "mainPage",
-    icon: NavbarHomeIcon,
-    activeIcon: NavbarHomeIconActive,
+    icon: {
+      default: NavbarHomeIcon,
+      active: NavbarHomeIconActive,
+    },
     text: "首頁",
   },
   {
     path: "user/self",
-    icon: NavbarUserIcon,
-    activeIcon: NavbarUserIconActive,
+    icon: {
+      default: NavbarUserIcon,
+      active: NavbarUserIconActive,
+    },
     text: "個人資料",
   },
   {
     path: "settings",
-    icon: NavbarSettingIcon,
-    activeIcon: NavbarSettingIconActive,
+    icon: {
+      default: NavbarSettingIcon,
+      active: NavbarSettingIconActive,
+    },
     text: "設定",
   },
 ];
@@ -34,6 +41,9 @@ const navItems = [
 const Navbar = () => {
   const [showModal, setShowModal] = useState(false);
   const [tweetText, setTweetText] = useState("");
+  const location = useLocation();
+  const { pathname } = location;
+  const [activeItem, setActiveItem] = useState(pathname.substring(1));
 
   const handleOpenModal = () => {
     setShowModal(true);
@@ -47,9 +57,6 @@ const Navbar = () => {
     console.log("提交推文:", tweetText);
     setTweetText("");
   };
-  const location = useLocation();
-  const { pathname } = location;
-  const [activeItem, setActiveItem] = useState(pathname.substring(1));
 
   const handleItemClick = (item) => {
     setActiveItem(item);
@@ -61,18 +68,16 @@ const Navbar = () => {
         <img src={NavbarLogo} alt="logo" />
       </div>
       <div className="navbarInfo">
-        {navItems.map(({ path, icon, activeIcon, text }) => (
-          <NavLink
-            exact={true.toString()}
-            to={`/${path}`}
+        {navItems.map(({ path, icon, text }) => (
+          <NavbarItem
             key={path}
-            className={`navbarItem ${activeItem === path ? "active" : ""}`}
-            activeClassName="active"
-            onClick={() => handleItemClick(path)}
-          >
-            <img src={activeItem === path ? activeIcon : icon} alt={path} />
-            <h5 className="medium">{text}</h5>
-          </NavLink>
+            path={path}
+            icon={icon.default}
+            activeIcon={icon.active}
+            text={text}
+            isActive={activeItem === path}
+            onClick={handleItemClick}
+          />
         ))}
         <div className="navbarItem navbarButton" onClick={handleOpenModal}>
           <button>推文</button>
