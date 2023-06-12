41b012034f96e60c3d729059cd76b877b31cdb85
kaihsu12 <kaihsu12@gmail.com>
2023-06-12 20:37:19 +0800

    replace svg files

--- src/component/navbar/Navbar.jsx
+++ src/component/navbar/Navbar.jsx
@@ -1,32 +1,48 @@
-import React, { useState } from "react";
-import { NavLink, useLocation } from "react-router-dom";
-import NavbarLogo from "../../assets/logo.svg";
-import NavbarHomeIcon from "../../assets/home.svg";
-import NavbarHomeIconActive from "../../assets/homeActive.svg";
-import NavbarUserIcon from "../../assets/userInfo.svg";
-import NavbarUserIconActive from "../../assets/userInfoActive.svg";
-import NavbarSettingIcon from "../../assets/setting.svg";
-import NavbarSettingIconActive from "../../assets/settingActive.svg";
-import "./Navbar.scss";
+import React, { useState } from 'react';
+import { NavLink, useLocation } from 'react-router-dom';
+import NavbarLogo from '../../assets/logo.svg';
+import NavbarHomeIcon from '../../assets/home.svg';
+import NavbarHomeIconActive from '../../assets/homeActive.svg';
+import NavbarUserIcon from '../../assets/userInfo.svg';
+import NavbarUserIconActive from '../../assets/userInfoActive.svg';
+import NavbarSettingIcon from '../../assets/setting.svg';
+import NavbarSettingIconActive from '../../assets/settingActive.svg';
+import NavbarLogoutIcon from '../../assets/logout.svg';
+import './Navbar.scss';
 
 const navItems = [
   {
-    path: "mainPage",
+    path: 'mainPage',
     icon: NavbarHomeIcon,
     activeIcon: NavbarHomeIconActive,
-    text: "首頁",
+    text: '首頁',
   },
   {
-    path: "user/self",
+    path: 'user/self',
     icon: NavbarUserIcon,
     activeIcon: NavbarUserIconActive,
-    text: "個人資料",
+    text: '個人資料',
   },
   {
-    path: "settings",
+    path: 'settings',
     icon: NavbarSettingIcon,
     activeIcon: NavbarSettingIconActive,
-    text: "設定",
+    text: '設定',
+  },
+];
+
+const adminNavItems = [
+  {
+    path: 'admin_main',
+    icon: NavbarHomeIcon,
+    activeIcon: NavbarHomeIconActive,
+    text: '推文清單',
+  },
+  {
+    path: 'admin_users',
+    icon: NavbarUserIcon,
+    activeIcon: NavbarUserIconActive,
+    text: '使用者列表',
   },
 ];
 
@@ -39,18 +55,20 @@ const Navbar = () => {
     setActiveItem(item);
   };
 
+  const loopMenuItems = activeItem.includes('admin') ? adminNavItems : navItems;
+
   return (
     <div className="navbar">
       <div className="navbar-logo">
         <img src={NavbarLogo} alt="logo" />
       </div>
       <div className="navbar-info">
-        {navItems.map(({ path, icon, activeIcon, text }) => (
+        {loopMenuItems.map(({ path, icon, activeIcon, text }) => (
           <NavLink
             exact="true"
             to={`/${path}`}
             key={path}
-            className={`navbar-item ${activeItem === path ? "active" : ""}`}
+            className={`navbar-item ${activeItem === path ? 'active' : ''}`}
             activeclassname="active"
             onClick={() => handleItemClick(path)}
           >
@@ -58,9 +76,15 @@ const Navbar = () => {
             <h5 className="medium">{text}</h5>
           </NavLink>
         ))}
+      </div>
+      {activeItem.includes('admin') ? null : (
         <div className="navbar-item navbar-button">
           <button>推文</button>
         </div>
+      )}
+      <div className="logoutBtn">
+        <img src={NavbarLogoutIcon} alt="" />
+        <h5 className="medium">登出</h5>
       </div>
     </div>
   );
