77f74dc9b854ef979e59b228338a0d642c4dfda7
kaihsu12 <kaihsu12@gmail.com>
2023-06-12 13:09:49 +0800

    create component and import UserCard

---
+++ src/page/UsersDashboard/UsersDashboard.jsx
@@ -0,0 +1,13 @@
+import UserCard from '../../component/userCard/UserCard';
+
+const UsersDashboard = () => {
+  return (
+    <div>
+      <ul>
+        <UserCard></UserCard>
+      </ul>
+    </div>
+  );
+};
+
+export default UsersDashboard;
