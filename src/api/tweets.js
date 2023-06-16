a61958661b06e0a1f4740a77458382e285f79b30
kaihsu12 <kaihsu12@gmail.com>
2023-06-16 21:05:39 +0800

    create async function to fetch specific tweet and replies for it

--- src/api/tweets.js
+++ src/api/tweets.js
@@ -40,3 +40,35 @@ export const postTweet = async ({ tweetText }) => {
     throw error;
   }
 };
+
+export const getSingleTweet = async (id) => {
+  try {
+    const res = await axiosInstance.get(`${baseUrl}/tweets/${id}`);
+    console.log(res.data);
+    return res.data;
+  } catch (error) {
+    console.error('[Get Tweet failed]: ', error);
+  }
+};
+
+export const getTweetReplies = async (id) => {
+  try {
+    const { data } = await axiosInstance.get(`${baseUrl}/tweets/${id}/replies`);
+    console.log(data);
+    return data;
+  } catch (error) {
+    console.error('[Get Tweet failed]: ', error);
+  }
+};
+
+export const postReply = async ({ id, comment }) => {
+  try {
+    const res = await axiosInstance.post(`${baseUrl}/tweets/${id}/replies`, {
+      comment,
+    });
+    return res;
+  } catch (error) {
+    console.error('[Post Reply failed]: ', error);
+    throw error;
+  }
+};
