5f3430f185153339a07ebf98e34ddcfdcb32e753
kaihsu12 <kaihsu12@gmail.com>
2023-06-16 21:07:47 +0800

    pass context down

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import MainPage from './page/mainPage/MainPage';
+import MainPage from './page/MainPage/MainPage';
 import UserSelf from './page/userSelfPage/UserSelf';
 import LoginPage from './page/LoginPage/LoginPage';
 import RegistPage from './page/RegistPage/RegistPage';
@@ -9,10 +9,11 @@ import UsersDashboard from './page/UsersDashboard/UsersDashboard';
 import AdminMain from './page/AdminMain/AdminMain';
 import HomePage from './page/HomePage/HomePage';
 import ReplyList from './page/ReplyList/ReplyList';
-import FollowAndFollower from "./page/followAndFollower/FollowAndFollower";
+import FollowAndFollower from './page/followAndFollower/FollowAndFollower';
 import './base.scss';
 import './reset.scss';
 import { AuthProvider } from './contexts/AuthContext';
+import { IdProvider } from './contexts/IdContext';
 
 /* import {LoginPage, MainPage, RegistPage} from './page' */
 function App() {
@@ -20,20 +21,22 @@ function App() {
     <Router>
       <div className="app">
         <AuthProvider>
-          <Routes>
-            <Route path="*" element={<HomePage />} />
-            <Route path="/mainPage" element={<MainPage />} />
-            <Route path="/user/self" element={<UserSelf />} />
-            <Route path="/follow" element={<FollowAndFollower />} />
-            <Route path="/follower" element={<FollowAndFollower />} />
-            <Route path="/login_admin" element={<LoginAdmin />} />
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/signup" element={<RegistPage />} />
-            <Route path="/settings" element={<SettingPage />} />
-            <Route path="/admin_main" element={<AdminMain />} />
-            <Route path="/admin_users" element={<UsersDashboard />} />
-            <Route path="/reply_list" element={<ReplyList />} />
-          </Routes>
+          <IdProvider>
+            <Routes>
+              <Route path="*" element={<HomePage />} />
+              <Route path="/mainPage" element={<MainPage />} />
+              <Route path="/user/self" element={<UserSelf />} />
+              <Route path="/follow" element={<FollowAndFollower />} />
+              <Route path="/follower" element={<FollowAndFollower />} />
+              <Route path="/login_admin" element={<LoginAdmin />} />
+              <Route path="/login" element={<LoginPage />} />
+              <Route path="/signup" element={<RegistPage />} />
+              <Route path="/settings" element={<SettingPage />} />
+              <Route path="/admin_main" element={<AdminMain />} />
+              <Route path="/admin_users" element={<UsersDashboard />} />
+              <Route path="/reply_list" element={<ReplyList />} />
+            </Routes>
+          </IdProvider>
         </AuthProvider>
         {/* 
         <AuthProvider>
