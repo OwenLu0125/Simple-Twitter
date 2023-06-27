d8f89ea621edebea6b4943a39650f2250fbda573
OwenLu <gg0125gg@gmail.com>
2023-06-27 16:54:29 +0800

    tem

--- src/page/SettingPage/SettingPage.jsx
+++ src/page/SettingPage/SettingPage.jsx
@@ -1,15 +1,35 @@
 import Navbar from "../../component/navbar/Navbar";
-import AuthInput from "../../component/authInput/AuthInput";
+// import AuthInput from "../../component/authInput/AuthInput";
 import PageTag from "../../component/pageTag/PageTag";
-import UserInputCollection from "../../component/userSetting/UserInputCollection";
+// import UserInputCollection from "../../component/userSetting/UserInputCollection";
+import UserSettingInput from "../../component/userSetting/userSettingInput/UserSettingInput";
 import "../SettingPage/SettingPage.scss";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../../contexts/AuthContext";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const SettingPage = () => {
+  const [account, setAccount] = useState("");
+  const [userName, setUserName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordCheck, setPasswordCheck] = useState("");
+
   const navigate = useNavigate();
   const { isAuthenticated } = useAuth();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const userChangeData = {
+      name: "user2set",
+      account: "user2set",
+      email: "user2set@example.com",
+      password: "12345678",
+      checkPassword: "12345678",
+    };
+    // console.log(userChangeData);
+  };
+
   useEffect(() => {
     if (!isAuthenticated) {
       navigate("/login");
@@ -22,7 +42,7 @@ const SettingPage = () => {
       </div>
       <div className="settingContainer">
         <PageTag title="帳戶設定" />
-        <form className="inputContainer">
+        <form className="inputContainer" onSubmit={handleSubmit}>
           {/* 這裡可能需要把所有輸入值，做成一個新元件，統一做管理 。
           TODO:
               3. 盤點需要的資料
@@ -33,7 +53,38 @@ const SettingPage = () => {
             1. 第一次渲染先從apiservice拿到資料
             2.                           
           */}
-          <UserInputCollection />
+          <UserSettingInput
+            label="帳號"
+            placeholder="請輸入帳號"
+            value={account}
+            onChange={(accountInputValue) => setAccount(accountInputValue)}
+          />
+          <UserSettingInput
+            label="名稱"
+            placeholder="請輸入使用者名稱"
+            value={userName}
+            onChange={(userNameInputValue) => setUserName(userNameInputValue)}
+          />
+          <UserSettingInput
+            label="Email"
+            placeholder="請輸入Email"
+            value={email}
+            onChange={(emailInputValue) => setEmail(emailInputValue)}
+          />
+          <UserSettingInput
+            type="password"
+            label="密碼"
+            placeholder="請設定密碼"
+            value={password}
+            onChange={(passwordInputValue) => setPassword(passwordInputValue)}
+          />
+          <UserSettingInput
+            type="password"
+            label="密碼確認"
+            placeholder="請再次輸入密碼"
+            value={passwordCheck}
+            onChange={(passwordCheck) => setPasswordCheck(passwordCheck)}
+          />
           <button className="btn">儲存</button>
         </form>
       </div>
